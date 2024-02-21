f944cf954c5108aaafd77629eed66c7629bd1708
Kaan <skrll4x3@hotmail.com>
2024-02-21 23:39:50 +0300

    loops

--- BreakContinue/BreakContinue/Program.cs
+++ BreakContinue/BreakContinue/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int counter=0; counter<10;counter++)
+            {
+               
+                if(counter %2 == 0)
+                {
+                    Console.WriteLine("Now comes an odd number");
+                    continue;
+
+                }
+                Console.WriteLine(counter);
+            }
+            Console.Read();
         }
     }
 }
