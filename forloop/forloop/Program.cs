e124fdc4cbb4f3c37d8253388b4cd12051033cdd
Kaan <skrll4x3@hotmail.com>
2024-02-21 22:59:12 +0300

    loops

---
+++ forloop/forloop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace forloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 50; counter +=5)
+            {
+                Console.WriteLine(counter + "is lower than 50 ");
+            }
+            Console.WriteLine("For loop is done");
+
+            for (int counter = 1; counter < 21; counter += 2)
+            {
+                Console.WriteLine(counter + " is lower than 50 ");
+            }
+            Console.ReadKey(); 
+
+        }
+    }
+}
