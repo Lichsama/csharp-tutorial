34e1a0c824c818fae520f81106d8f336d0e714f3
Kaan <skrll4x3@hotmail.com>
2024-03-28 15:44:41 +0300

    a

---
+++ Readfromtext/Readfromtext/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Readfromtext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\skrll\OneDrive\Masaüstü\textFile.txt");
+            Console.WriteLine("Textfile contains following text: {0}",text);
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\skrll\OneDrive\Masaüstü\textFile.txt");
+            Console.WriteLine("Contents of textfile.txt = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+        }
+    }
+}
