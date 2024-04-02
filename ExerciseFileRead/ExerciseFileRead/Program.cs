db706f13464d382d997cd701e927cc9461ac67e6
Kaan <skrll4x3@hotmail.com>
2024-04-02 23:13:01 +0300

    practice

---
+++ ExerciseFileRead/ExerciseFileRead/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.IO;
+
+
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    
+    {
+
+        // Read the file line by line
+
+        string[] lines = File.ReadAllLines(@"C:\Users\skrll\Downloads\input.txt");
+
+
+
+        // Create a StreamWriter for the output file
+
+        using (StreamWriter sw = new StreamWriter(@"C:\Users\skrll\Downloads\output.txt"))
+
+        {
+
+            foreach (string line in lines)
+
+            {
+
+                // Check if the line contains "split"
+
+                if (line.Contains("split"))
+
+                {
+
+                    // Split the line and get the element at index 4
+
+                    string[] splitLine = line.Split(' ');
+
+                    if (splitLine.Length > 4)
+
+                    {
+
+                        // Write the element to the output file
+
+                        sw.Write(splitLine[4] + " ");
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ RandomClassC/RandomClassC/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace RandomClassC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your question!");
+            Console.ReadLine();
+
+            Random dice = new Random();
+            int numEyes;
+            Random yesNoMaybe=new Random();
+            int answerNum;
+            answerNum=yesNoMaybe.Next(1,4);
+
+            if (answerNum == 1)
+            {
+                Console.WriteLine("Yes");
+            }else if(answerNum == 2)
+            {
+                Console.WriteLine("Maybe");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            for(int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 7);
+                Console.WriteLine(numEyes);
+            }
+            Console.Read();
+
+
+
+
+        }
+
+    }
+}
